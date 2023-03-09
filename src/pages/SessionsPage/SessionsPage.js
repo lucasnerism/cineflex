17ad7e30c042213bc910af15fba3899115b0106e
Lucas Neris <lucasnerism@gmail.com>
2023-03-09 18:33:05 -0300

    Tela de escolha de sessão implementada

--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -33,30 +33,14 @@ export default function SessionsPage() {
             <div>
                 {movie.days.map(ses => (
                     <SessionContainer>
-                        Sexta - 03/03/2023
+                        {`${ses.weekday} - ${ses.date}`}
                         <ButtonsContainer>
-                            <button>14:00</button>
-                            <button>15:00</button>
+                            {ses.showtimes.map(time => (
+                                <Link to={`/sessao/${time.id}`} ><button>{time.name}</button></Link>
+                            ))}
                         </ButtonsContainer>
                     </SessionContainer>
                 ))}
-
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
-
-                <SessionContainer>
-                    Sexta - 03/03/2023
-                    <ButtonsContainer>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                    </ButtonsContainer>
-                </SessionContainer>
             </div>
 
             <FooterContainer>
